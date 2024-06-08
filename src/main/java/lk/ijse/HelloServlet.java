29a2ad491ae70925e753e7cc04302c768cf567bf
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2024-06-08 17:28:13 +0530

    xml created.

---
+++ src/main/java/lk/ijse/HelloServlet.java
@@ -0,0 +1,17 @@
+package lk.ijse;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        System.out.println("Incoming request " + req.getRemoteAddr());
+        resp.getWriter().println("Hello I'm from HelloServlet");
+    }
+
+}
